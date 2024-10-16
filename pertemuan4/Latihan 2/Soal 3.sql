00ee2a657f58e44965e0a9544c4057dec25e1330
Nndaaa404 <anandaarizkymaulana@gmail.com>
2024-10-16 15:38:18 +0700

    Latihan 2

---
+++ pertemuan4/Latihan 2/Soal 3.sql
@@ -0,0 +1,3 @@
+SELECT count(*) AS jumlah_total_mahasiswa_informatika
+FROM mahasiswa 
+WHERE jurusan = 'Teknik Informatika';
---
+++ pertemuan4/Latihan 2/Soal 9.sql
@@ -0,0 +1,4 @@
+Select nama_mata_kuliah
+From jadwal_mata_kuliah
+Where dosen_pengajar Like '%ang%'
+ORDER BY nama_mata_kuliah asc;
